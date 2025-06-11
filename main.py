0021aca76395382ae77346ef4e65624ce9d55c56
01Petard <1520394133@qq.com>
2025-06-11 22:30:41 +0800

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = ["requests"]
+# ///
+
+
+import requests
+
+def main():
+    print(requests.__version__)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
